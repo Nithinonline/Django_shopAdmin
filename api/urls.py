5dd1b9a6c10b51b8867365e8fba00554a6d7d15b
Nithin Suresh <webdevnithin@gmail.com>
2024-05-08 10:24:22 +0530

    Requirements.txt Added

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,21 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import shopViewSet
+
+
+router=DefaultRouter()
+router.register(r'shops',shopViewSet)
+
+# urlpatterns = [
+#     path('',views.ApiOverview,name='home'),
+#     path('create/',views.add_shop_details,name='add-shop'),
+#     path('all/',views.view_shop,name='view-shops'),
+#     path('update/<int:pk>/',views.update_shop_details,name='update-shop'),
+#     path('delete/<int:pk>/',views.delete_shop,name='delete-shop')
+# ]
+
 
 urlpatterns = [
-    path('',views.ApiOverview,name='home'),
-    path('create/',views.add_shop_details,name='add-shop'),
-    path('all/',views.view_shop,name='view-shops'),
-    path('update/<int:pk>/',views.update_shop_details,name='update-shop'),
-    path('delete/<int:pk>/',views.delete_shop,name='delete-shop')
+    path('',include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,4 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
 
-# def index(request):
-#     return HttpResponse("Hello Admin")
 
 
 from rest_framework.decorators import api_view,permission_classes
@@ -13,99 +9,118 @@ from rest_framework import status,serializers
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets
  
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def ApiOverview(request):
-    api_urls = {
-        'all_items': '/',
-        'Search by name': '/?name=shop_name',
-        'Search by city': '/?city=city_name',
-        'Add': '/create',
-        'Update': '/update/pk',
-        'Delete': '/item/pk/delete'
-    }
- 
-    return Response(api_urls)
-
-#Adding new shop
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def add_shop_details(request):
-    shop=ShopSerializer(data=request.data)
-    print(shop)
-
-    #validating for already existing data
-    if Shop.objects.filter(**request.data).exists():
-        raise serializers.ValidationError('This data already exists')
-    
-    if shop.is_valid():
-        shop.save()
-        return Response(shop.data)
-    else:
-        print(shop.errors)
-        return Response(status=status.HTTP_400_BAD_REQUEST,)
-    
-
-
-
-#view shop
-@api_view(['GET'])
+# @api_view(['GET'])
 # @permission_classes([IsAuthenticated])
-def view_shop(request):
+# def ApiOverview(request):
+#     api_urls = {
+#         'all_items': '/',
+#         'Search by name': '/?name=shop_name',
+#         'Search by city': '/?city=city_name',
+#         'Add': '/create',
+#         'Update': '/update/pk',
+#         'Delete': '/item/pk/delete'
+#     }
+ 
+#     return Response(api_urls)
 
-    paginator=PageNumberPagination()
-    paginator.page_size=5
+# #Adding new shop
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def add_shop_details(request):
+#     shop=ShopSerializer(data=request.data)
+#     print(shop)
 
-    # if request.query_params:
-    #     shop=Shop.objects.filter(**request.query_params.dict())
-
-    # else:
-    shop=Shop.objects.all()
-
-    # if shop:
-    #     serializer=ShopSerializer(shop,many=True)
-    #     return Response(serializer.data)
-    result_page=paginator.paginate_queryset(shop,request)
-    serializer=ShopSerializer(result_page,many=True)
-
-    # else:
-    return paginator.get_paginated_response(serializer.data)
-
-
-#update shop details
-@api_view(['PATCH'])
-@permission_classes([IsAuthenticated])
-def update_shop_details(request,pk):
-    try:
-       shop=Shop.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND,data="Error finding shop")
-    if shop.user==str(request.user):
-       data=ShopSerializer(instance=shop,data=request.data,partial=True)
-       if data.is_valid():
-          data.save()
-          return Response(data.data)
-       else:
-          return Response(data.errors,status=status.HTTP_400_BAD_REQUEST)
-       
-    else:
-        return Response({"error":"you are not allowd to update"},status=status.HTTP_403_FORBIDDEN)   
+#     #validating for already existing data
+#     if Shop.objects.filter(**request.data).exists():
+#         raise serializers.ValidationError('This data already exists')
+    
+#     if shop.is_valid():
+#         shop.save()
+#         return Response(shop.data)
+#     else:
+#         print(shop.errors)
+#         return Response(status=status.HTTP_400_BAD_REQUEST,)
     
 
-#Delete shop
-@api_view(['DELETE'])
-@permission_classes([IsAuthenticated])
-def delete_shop(request, pk):
-    try:
-        shop = Shop.objects.get(pk=pk)
-        shop.delete()
-        return Response(data={"message": "Deleted successfully"})
-    except ObjectDoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)  
+
+
+# #view shop
+# @api_view(['GET'])
+# # @permission_classes([IsAuthenticated])
+# def view_shop(request):
+
+#     paginator=PageNumberPagination()
+#     paginator.page_size=5
+
+#     # if request.query_params:
+#     #     shop=Shop.objects.filter(**request.query_params.dict())
+
+#     # else:
+#     shop=Shop.objects.all()
+
+#     # if shop:
+#     #     serializer=ShopSerializer(shop,many=True)
+#     #     return Response(serializer.data)
+#     result_page=paginator.paginate_queryset(shop,request)
+#     serializer=ShopSerializer(result_page,many=True)
+
+#     # else:
+#     return paginator.get_paginated_response(serializer.data)
+
+
+# #update shop details
+# @api_view(['PATCH'])
+# @permission_classes([IsAuthenticated])
+# def update_shop_details(request,pk):
+#     try:
+#        shop=Shop.objects.get(pk=pk)
+#     except:
+#         return Response(status=status.HTTP_404_NOT_FOUND,data="Error finding shop")
+#     if shop.user==str(request.user):
+#        data=ShopSerializer(instance=shop,data=request.data,partial=True)
+#        if data.is_valid():
+#           data.save()
+#           return Response(data.data)
+#        else:
+#           return Response(data.errors,status=status.HTTP_400_BAD_REQUEST)
+       
+#     else:
+#         return Response({"error":"you are not allowd to update"},status=status.HTTP_403_FORBIDDEN)   
+    
+
+# #Delete shop
+# @api_view(['DELETE'])
+# @permission_classes([IsAuthenticated])
+# def delete_shop(request, pk):
+#     try:
+#         shop = Shop.objects.get(pk=pk)
+#         shop.delete()
+#         return Response(data={"message": "Deleted successfully"})
+#     except ObjectDoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)  
 	
 
 
 
+##########################################################################################################################
+
+#Class Based View
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 5 
+    page_size_query_param = 'page'
+    max_page_size = 100
+
+class shopViewSet(viewsets.ModelViewSet):
+    queryset=Shop.objects.all()
+    serializer_class=ShopSerializer
+    permission_classes=[IsAuthenticated]
+    pagination_class=CustomPagination
+
+   
+   
 
 
